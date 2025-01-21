6114bd46e403a95781e7e93216e062b6f64fcfd3
ThanhBr <thanhmt>
2025-01-21 09:33:56 +0700

    Exception Handling

--- src/main/java/spring/api/identity_service/repository/UserRepository.java
+++ src/main/java/spring/api/identity_service/repository/UserRepository.java
@@ -6,4 +6,5 @@ import spring.api.identity_service.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByUsername(String username);
 }
